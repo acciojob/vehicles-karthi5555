3fe088c833c34e69669a2eae4d3f07908810bcb6
Karthik M <karthikm7483@gmail.com>
2023-09-08 11:50:19 +0530

    Code is reused

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,10 +2,18 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
-        mercedes.accelerate(20);
-        mercedes.accelerate(-60);
+//        F1 mercedes = new F1("mercedes", false);
+//        mercedes.move(40, 0);
+//        mercedes.accelerate(20);
+//        mercedes.accelerate(-60);
+
+        //for my test cases
+        F1 benz = new F1("benz",false);
+        benz.move(50, 20);
+        benz.accelerate(30);
+        benz.accelerate(30);
+        benz.stop();
+        System.out.println(benz.getCurrentSpeed());
 
     }
 }
